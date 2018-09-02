4588370814b4286bad437d0cdef7ce91f31d4d8b
Alexander <alexandermlinzk@gmail.com>
2018-09-02 21:12:31 +0200

    Added deploy options stage

--- vars/publishDeployConfig.groovy
+++ vars/publishDeployConfig.groovy
@@ -1,3 +1,20 @@
+import groovy.json.JsonBuilder
+
 def call(config) {
+
+    stage("Publish deploy config") {
+        String[] whitelistServers = config.serverWhitelist
+        String[] blacklistServers = config.serverBlacklist
+
+        def builder = new JsonBuilder()
+        def root = builder deployable: config.success, serverWhitelist: whitelistServers, serverBlacklist: blacklistServers
+
+        echo 'Writing deploy options to target directory'
+
+        node {
+            writeJSON file: 'target/deployOptions.json', json: root.toString()
+            archiveArtifacts artifacts: 'target/deployOptions.json', fingerprint: true, onlyIfSuccessful: true
+        }
+    }
 
 }
