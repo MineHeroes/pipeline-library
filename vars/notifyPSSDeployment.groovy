ec2f32074c0fce50753434bdc1cc0545a20cf6d9
Alexander <alexandermlinzk@gmail.com>
2018-09-02 17:01:37 +0200

    Don't refresh jenkins cache if no address was given

--- vars/notifyPSSDeployment.groovy
+++ vars/notifyPSSDeployment.groovy
@@ -1,6 +1,10 @@
 def call(config) {
-    echo 'Triggering Jenkins refresh'
     String[] addresses = config.pssAddresses
+    if (addresses.length == 0) {
+        return
+    }
+
+    echo 'Triggering Jenkins refresh'
     for (int i = 0; i < addresses.length; i++) {
         sh 'curl --data "jobtype=jenkins.branch.OrganizationFolder&jobname=Mineheroes" ${addresses[i]} &'
     }
