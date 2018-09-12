7067a412e103aa2ed245792010f641a81ed9ce26
Alexander <alexandermlinzk@gmail.com>
2018-09-12 14:14:15 +0200

    use concat method to build mvn command

--- vars/stepBuildJar.groovy
+++ vars/stepBuildJar.groovy
@@ -4,22 +4,22 @@ def call(config) {
         echo 'Starting build and deploy process.'
         timeout(time: 20, unit: 'MINUTES') {
             def pom = readMavenPom file: 'pom.xml'
-            def command = "MAVEN_OPTS=\"-XX:+TieredCompilation -XX:TieredStopAtLevel=1\" mvn -T 2C"
+            def command = 'MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1" mvn -T 2C'
 
             if (config.javadoc == true) {
                 echo 'Including JavaDoc'
-                command = "${command} javadoc:jar"
+                command = command.concat(' javadoc:jar');
             }
             if (config.source == true) {
                 echo 'Including Source'
-                command = "${command} source:jar"
+                command = command.concat(' source:jar')
             }
-            command = "${command} -Dbuild.number=-${BRANCH_NAME}_B${env.BUILD_NUMBER}"
+            command = command.concat(" -Dbuild.number=-${BRANCH_NAME}_B${env.BUILD_NUMBER}")
             if (pom.getDistributionManagement() != null) {
                 echo 'Deploying results to Nexus'
-                command = "${command} deploy"
+                command = command.concat(' deploy')
             } else {
-                command = "${command} install"
+                command = command.concat(' install')
             }
 
             sh "${command}"
