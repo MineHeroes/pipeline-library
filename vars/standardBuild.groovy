e2dda1c495c3d862ab44508833a263f26f897e96
Alexander <alexandermlinzk@gmail.com>
2020-11-03 12:27:56 +0100

    Use maven from the global tool configuration

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -6,6 +6,14 @@ class standardBuild extends baseBuild {
     def process(config) {
         node {
             properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: config.keepNumberOfBuilds, daysToKeepStr: '', numToKeepStr: ''))])
+
+            //add maven from /opt/maven/bin from the tool config to path
+            env.MVN_HOME = "${tool name: 'Opt Maven', type: 'maven'}"
+            env.PATH = "${env.MVN_HOME}bin:${env.PATH}"
+
+            echo 'Using Maven Version:'
+            sh 'mvn --version'
+
             stage('Checkout') {
                 echo 'Checking out SCM'
                 checkout scm
